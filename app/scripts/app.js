07cdf0b52bd511a2603ed1a5689545cdebfbca1b
syamsulmaarif <syammsul.maarif94@gmail.com>
2015-03-08 12:38:34 +0700

    initial project

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name belajarAngularApp
+ * @description
+ * # belajarAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('belajarAngularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name belajarAngularApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the belajarAngularApp
+ */
+angular.module('belajarAngularApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
